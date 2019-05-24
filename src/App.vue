89f794d133c7bb1a12b1defd6a7e7f123e05a69f
kitanotamayura2357 <kitanotamayura2357@gmail.com>
2019-05-24 10:38:56 +0900

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -5,55 +5,73 @@
     </h1>
     <button
       class="start-game"
+      v-on:click="startGame"
     >
       Start Game
     </button>
     <div class="counters-container">
-      <div class="counter">
-        <h2>Score:</h2>
-        <h1>0</h1>
-      </div>
-      <div class="counter">
-        <h2>High Score:</h2>
-        <h1>0</h1>
-      </div>
-      <div class="counter">
-        <h2>Timer:</h2>
-        <h1>0</h1>
-      </div>
+      <Counter label="Score:" v-bind:value="score"></Counter>
+      <Counter label="High-Score:" v-bind:value="highscore"></Counter>
+      <Counter label="Timer" v-bind:value="timer"></Counter>
     </div>
-    <div class="moles-container gameActive">
-      <div class="mole-container inactive">
-        <div class="mole-image-container">
-          <img class="mole" src="./assets/mole.png" alt="mole"/>
-        </div>
-        <img class="dirt" src="./assets/dirt.svg" alt="mole dirt"/>
-      </div>
-      <div class="mole-container inactive">
-        <div class="mole-image-container">
-          <img class="mole" src="./assets/mole.png" alt="mole"/>
-        </div>
-        <img class="dirt" src="./assets/dirt.svg" alt="mole dirt"/>
-      </div>
-      <div class="mole-container inactive">
-        <div class="mole-image-container">
-          <img class="mole" src="./assets/mole.png" alt="mole"/>
-        </div>
-        <img class="dirt" src="./assets/dirt.svg" alt="mole dirt"/>
-      </div>
-      <div class="mole-container inactive">
-        <div class="mole-image-container">
-          <img class="mole" src="./assets/mole.png" alt="mole"/>
-        </div>
-        <img class="dirt" src="./assets/dirt.svg" alt="mole dirt"/>
-      </div>
-    </div>
+    <!-- <Moles></Moles> -->
+    <Moles
+    v-bind:moleData="moles"
+    v-bind:gameActive="gameActive"
+    />
   </div>
 </template>
 
 <script>
+import Counter from './components/Counter';
+import Moles from './components/Moles';
+
 export default {
   name: 'App',
+  components: { 
+    Counter, // "Counter": Counter,
+    Moles // "Moles": Moles,
+  },
+  data: function() {
+    return{
+      score: 10,
+      highscore: 0,
+      timer: 20,
+      moles: [false, false, false, false],
+      gameActive: false,
+    };
+  },
+
+  methods: {
+    resetState: function() {
+      this.score = 0;
+      this.timer = 20;
+      this.moles = [false,false,false,false];
+    },
+    startGame: function() {
+      this.resetState();
+      this.gameActive = true;
+      this.startTimer();
+    },
+    endGame: function() {
+      this.gameActive = false;
+      this.stopTimer();
+    },
+    startTimer: function() {
+      this.timerId = setInterval(() => {
+        this.decrementTime();
+      }, 1000);
+    },
+    decrementTime: function() {
+      this.timer--;
+      if (this.timer === 0) {
+        this.endGame();
+      }
+    },
+    stopTimer: function() {
+      clearInterval(this.timerId);
+    },
+  },
 };
 </script>
 
@@ -81,69 +99,14 @@ export default {
   justify-content: space-evenly;
 }
 
-.counter {
-  border: 1px solid #000;
-  margin-top: 20px;
-  padding: 20px;
-}
 
-.counter h1,
-.counter h2 {
-  margin: 0;
-}
 
-.moles-container {
-  margin-top: 20px;
-  display: flex;
-  justify-content: space-between;
-  opacity: 0.5;
-  transition: opacity 0.3s ease;
-}
 
-.moles-container.game-active {
-  opacity: 1;
-}
-
-.mole-container {
-  width: 160px;
-  height: 160px;
-  display: inline-block;
-  margin: 10px;
-  position: relative;
-}
-
-.mole-image-container {
-  overflow: hidden;
-  width: 160px;
-  height: 140px;
-}
-
-.mole-container img {
-  display: block;
-  transition: all 0.3s ease;
-}
-
-.mole {
-  position: relative;
-  width: 60%;
-  margin: auto;
-  cursor: pointer;
-}
 
 .mole-container.active .mole {
   /* display: block; */
   top: 0px;
 }
 
-.mole-container.inactive .mole {
-  top: 200px;
-}
 
-.dirt {
-  width: 100%;
-  margin: auto;
-  z-index: 1;
-  position: absolute;
-  bottom: 0;
-}
 </style>
---
+++ src/components/Moles.vue
@@ -0,0 +1,43 @@
+<template>
+<div v-bind:class="classNames">
+    <Mole
+        v-for="(moleState, idx) in moleData"
+        v-bind:key="idx"
+        v-bind:active="moleState"
+    >
+    </Mole>
+</div>
+</template>
+
+<script>
+ import Mole from './Mole';
+ export default {
+     name: 'Moles',
+     components:{
+         Mole,
+     },
+     props: ['moleData', 'gameActive'],
+     computed: {
+         classNames: function() {
+             return {
+                 'moles-container': true,
+                 'game-active': this.gameActive,
+             };
+         }
+     },
+ };
+</script>
+
+<style>
+.moles-container.game-active {
+  opacity: 1;
+}
+
+.moles-container {
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+  opacity: 0.9;
+  transition: opacity 0.3s ease;
+}
+</style>
